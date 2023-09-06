425b626572f009b377a36ba0d14d81bff5ca2141
Alexis Medina <giovanin007@gmail.com>
2023-09-05 19:36:34 -0500

    inicio

---
+++ introduccion/variables.js
@@ -0,0 +1,22 @@
+/* Tipos de datos que podemos guardar en las variables:
+string - Cadena de texto
+number - Numeros
+boolean - Booleano (Verdadero o Falso)
+object - Objeto
+function - Funciones
+
+null - Valor nulo
+undefined - Valor sin definir
+*/
+
+//let edad;
+//console.log(17);
+
+//let nombre = 'Alexis';
+//console.log(nombre);
+
+//const correo = 'correo@correo.com';
+//console.log(correo);
+
+const mayorQue = 1 > 2;
+console.log(mayorQue);
